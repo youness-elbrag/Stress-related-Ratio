22dcd322ff00072005cbc1537cd527e654128f81
yunss-ML <younsselbrg@gmail.com>
2023-08-14 13:40:15 +0100

    simulate the stress and strain diaggram

---
+++ README.md
@@ -0,0 +1,45 @@
+# Mechanics and Materials Introduction
+
+Here the Overview of Stree and Strain study within code in Python
+
+
+## Introduction
+
+In the materials science and mechanics, comprehending how materials react to external forces is paramount. This comprehension forms the foundation for predicting material behavior under various circumstances, which is indispensable for engineering design and analysis. In this section, we will revisit the core concepts of stress, strain, and the mechanical behavior of materials.
+
+## Stress and Strain
+
+**Stress** is a pivotal mechanical property that characterizes how internal forces are distributed across a material's cross-section. It quantifies the intensity of these internal forces per unit area and is measured in N/m² (Pascals). Stress can be calculated using the equation:
+
+$\[ \sigma = \frac{Force (F)}{Area (A)} \]$
+
+Stress is crucial for understanding how materials respond to external loads and is a critical parameter for assessing strength and stability. It can be both tensile (stretching) and compressive (shortening) in nature.
+
+**Strain** is a measure of the deformation that occurs within a material in response to applied forces or loads. It quantifies the change in size or shape of a material relative to its original dimensions.
+
+## Predicting Object Failure
+
+One of the advantage studying of stress analysis is predicting when an object will fail. For instance, consider a scenario where the maximum pressure applied to an object is $\( \sigma_y = 250 \, \text{MPa} \)$, which represents the failure stress. If we know $the cross-sectional area (\( A \))$ of the object, we can compute the force $(\( F \))$ required to induce this failure stress using the equation:
+
+$\[ F = \sigma_y \times A \]$
+
+## Stress-Strain Relationship
+
+The interconnection between stress and strain is fundamental to grasping how materials perform under load. This relationship is conventionally depicted using a **stress-strain diagram**, a graphical representation of a material's response to escalating stress. The stress-strain diagram provides insights into a material's elastic behavior, yield point, plastic deformation, and ultimate failure.
+
+![Stress-Strain Diagram](output.png)
+
+## Hooke's Law
+
+One of the primary principles governing the elasticity of materials is **Hooke's Law**, which asserts that the deformation (strain) of a material is directly proportional to the applied stress within the elastic limit. Mathematically, Hooke's Law can be formulated as:
+
+$\[ \sigma = E \cdot \epsilon \]$
+
+$where:
+- \( \sigma \) represents the stress applied to the material.
+- \( E \) denotes the Young's Modulus or Elastic Modulus, a material property indicating its stiffness.
+- \( \epsilon \) signifies the strain experienced by the material.$
+
+Hooke's Law is a fundamental concept for comprehending the initial behavior of materials within their elastic range. Beyond this point, materials undergo plastic deformation and might not return to their original state upon unloading.
+
+
---
+++ computeRatio.py
@@ -0,0 +1,124 @@
+'''
+Developed by:
+Moin Khan
+Python 3.0
+'''
+# import matplotlib
+# matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+import numpy as np
+from scipy.interpolate import interp1d
+from matplotlib.offsetbox import AnchoredText
+
+fig = plt.figure()
+ax = fig.add_subplot(1,1,1)
+
+# define your values
+Force = [0,4400,5670,6600,7040,8320,8800,9200,9240]
+Elongation = [0,1,2,4,5,8,10,15,18]
+GLength = 75
+Radius = 6.65
+LinearLimit = 1
+
+
+#area calculation
+Area = math.pi*Radius*Radius
+
+
+#stress strain calculated as per above values
+Stress = [ x*9.8/Area for x in Force ]
+Strain = [ x/GLength for x  in Elongation ]
+
+#True Stress calculation
+Stress_True = [ x * (1+y) for y,x in zip(Strain,Stress)]
+#True Strain calculation
+Strain_True = [math.log(1+x) for x in Strain]
+
+
+#values created which are needed for interpolation
+Strain_values_linear = np.linspace(Strain[0], Strain[LinearLimit], num=41, endpoint=True)
+Strain_values_eng = np.linspace(Strain[LinearLimit], Strain[-1], num=41, endpoint=True)
+Strain_values_true = np.linspace(Strain_True[LinearLimit], Strain_True[-1], num=41, endpoint=True)
+
+#interpolation defined
+f1 = interp1d(Strain, Stress, fill_value='extrapolate')
+f2 = interp1d(Strain, Stress, kind=3, fill_value='extrapolate')
+f3 = interp1d(Strain_True, Stress_True, kind=3)
+
+#plot
+ax.plot(Strain,Stress, 'o')
+ax.plot(Strain_values_linear, f1(Strain_values_linear),'b-')
+ax.plot(Strain_values_eng, f2(Strain_values_eng),'g-')
+ax.plot(Strain_values_true, f3(Strain_values_true),'r-')
+
+#set label
+ax.set_xlabel('Strain')
+ax.set_ylabel('Stress  (MPa)')
+
+#define ElasticLimit
+ElasticLimit = Stress[LinearLimit] 
+#define Modulus
+Slope = Stress[LinearLimit]/Strain[LinearLimit]
+Modulus = Slope/math.pow(10,3)
+#define UTS
+uts = max(Stress)
+#define Failure Stress
+failure_stress = f2(max(Strain))
+
+#plot UTS Line
+plt.axhline(y=uts, ls=':', c='cyan')
+#plot Ductility Line
+plt.axvline(x=Strain[-1], ls=':', c='purple')
+
+#plot offset Line
+xA = [Strain[0],Strain[LinearLimit]]
+yA = [Stress[0],Stress[LinearLimit]]
+Strain_values_offset = [x +.002*Strain[-1] for x in xA]
+f4 = interp1d(Strain_values_offset, yA, fill_value='extrapolate')
+Strain_values_offset.append(Strain[LinearLimit+1])
+ax.plot(Strain_values_offset,f4(Strain_values_offset),':',color='orange')
+
+
+#find offset yield value              
+val=Strain[LinearLimit]
+step = (Strain[LinearLimit+1]-Strain[LinearLimit])/50
+sign = f4(val)-f2(val)
+while(1):
+    if((f4(val)-f2(val))*sign < 0):
+         break    
+    val = val+ step
+YieldPoint = f4(val-step)
+
+#plot offset horizontal line
+plt.axhline(y=YieldPoint, ls=':', c='black')
+
+
+#plt legend
+plt.legend(['Actual Values', 'Linear Region ', 'Engineering Stress Strain','True Stress Strain','UTS','Max Strain','Offset Line','Yield Point'], loc='best')
+#add anchor
+anchored_text = AnchoredText("Young's Modulus = " +"%.5f" % Modulus + " GPa\n" +
+                             "Elastic Limit = " + "%.5f" % ElasticLimit + " MPa\n" +
+                             "Yield Stress = "+ "%.5f" % YieldPoint + " MPa\n"+
+                             "UTS = "+ "%.5f" % uts +" MPa\n"+
+                             "Failure Stress = " + "%.5f" % failure_stress +" MPa\n"+
+                             "Max Strain = "+ "%.5f" % Strain[8], loc='right')
+ax.add_artist(anchored_text)
+
+#set limits
+ax.set_xlim(xmin=0)
+ax.set_ylim(ymin=0)
+
+#display graph
+plt.savefig("output.png")
+plt.close()
+
+#print(Force)
+#print(Elongation)
+#print(Stress)
+#print(Strain)
+#print(Stress_True)
+#print(Strain_True)
+#print(Area)
+#
